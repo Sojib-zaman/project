9ae4584bac4a4136437faef2ea0138b3d4e8dff5
Sojib-zaman <68711914+Sojib-zaman@users.noreply.github.com>
2022-08-25 13:48:06 +0600

    Add files via upload

--- backend/Routes/member.js
+++ backend/Routes/member.js
@@ -20,6 +20,45 @@ router.post('/signup' , async(req , res) =>
 }
 )
 
+router.post('/update' , async(req , res) =>
+{
+    try
+    {
+        console.log("in member update") ; 
+       
+        const {NAME , PASSWORD , COUNTRY , EMAIL , IMAGE , ID} = req.body ; 
+        console.log(NAME , PASSWORD , COUNTRY , EMAIL , IMAGE , ID)
+        const result = await query.update( NAME , PASSWORD , COUNTRY , EMAIL , IMAGE , ID) 
+       
+        res.end() ; 
+    }
+    catch(err)
+    {
+        console.log(err)  ;
+    }
+}
+)
+
+router.post('/delete' , async(req , res) =>
+{
+    try
+    {
+        console.log("in member delete") ; 
+       
+        const { ID , EMAIL} = req.body ; 
+        console.log( ID)
+        const result = await query.delete(  ID, EMAIL) 
+       
+        res.end() ; 
+    }
+    catch(err)
+    {
+        console.log(err)  ;
+    }
+}
+)
+
+
 router.post('/login' , async(req , res)=>
 {
    
@@ -41,6 +80,45 @@ router.post('/login' , async(req , res)=>
 }
 )
 
+router.get('/user/:ID' , async(req , res )=>
+{
+    
+    try 
+    {
+        const ID =  await req.params.ID ; 
+        console.log("ID:")
+        console.log(ID) ; 
+        const result = await query.getuser(ID) ;
+        console.log(result) 
+        res.json(result) ; 
+    }
+    catch(error)
+    {
+        console.log(error) ; 
+    }
+})
+
+
+router.post('/isdeleted' , async(req , res)=>
+{
+   
+    try
+    {
+        console.log("in member login isdeleted") ; 
+       console.log(req.body) ; 
+       
+       const { EMAIL } = req.body ; 
+       const result = await query.isdel(EMAIL) ; 
+
+       console.log(result) ; 
+       res.json(result) ;
+    }
+    catch(err)
+    {
+        console.log(err) ; 
+    }
+}
+)
 
 
 module.exports = router  ; 
--- backend/register_log/create.js
+++ backend/register_log/create.js
@@ -33,6 +33,78 @@ handle.verify = async(email , password)=>
     return result ; 
 }
 
+handle.isdel = async(email)=>
+{
+    
+//     const query = `
+//    SELECT * FROM IS_DELETED(:email)
+ 
+//     `
 
+ const query=`SELECT * FROM DELETED_USER WHERE EMAIL=:email`
+    console.log("here in isdel")
+    console.log(email)  ; 
+    const binds = {email} 
+    const result = (await con.execute(query , binds , con.options))
+    console.log(result) ; 
+    return result ; 
+}
+
+
+
+
+handle.getuser = async(ID)=>
+{
+    const query = `SELECT * FROM C##PROJECT.APP_USER WHERE ID = :ID `
+    console.log("here in create GETUSER")
+    console.log(ID)  ; 
+    const binds = {ID:ID } 
+    const result = (await con.execute(query , binds , con.options)).rows
+    console.log(result) ; 
+    return result ; 
+}
+
+handle.update = async(NAME , PASSWORD , COUNTRY , EMAIL , IMAGE , ID ) => 
+{
+    console.log(NAME , PASSWORD , COUNTRY , EMAIL , IMAGE, ID  ) 
+   
+    const query = ` UPDATE APP_USER
+    SET NAME = :NAME , PASSWORD= :PASSWORD , EMAIL = :EMAIL , IMAGE= :IMAGE , COUNTRY = :COUNTRY 
+    WHERE ID = :ID
+    `
+
+    const binds = {NAME , PASSWORD , COUNTRY , EMAIL , IMAGE , ID  }
+
+    const result = (await con.execute(query , binds , con.options)).rows
+   // console.log(result) ;
+
+    return result ; 
+}
+
+
+handle.delete = async(ID,EMAIL ) => 
+{
+    console.log(ID,EMAIL) 
+   
+    const query = `  DELETE FROM APP_USER 
+    WHERE ID = :ID
+   
+    `
+
+    const binds = {ID}
+
+    const result = (await con.execute(query , binds , con.options))
+  
+    const query2 = `    INSERT INTO C##PROJECT.DELETED_USER(EMAIL) VALUES(:EMAIL)
+    `
+
+    const binds2 = {EMAIL}
+
+    const result2= (await con.execute(query2 , binds2, con.options))
+
+    console.log(result) ;
+
+    return result ; 
+}
 
 module.exports = handle ; 
